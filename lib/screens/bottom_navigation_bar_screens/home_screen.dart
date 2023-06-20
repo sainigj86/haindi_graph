f8f6b3d0e5d9e67c0be8b886560435e984a46ff7
Gajendra saini <sainigj86@gmail.com>
2023-06-20 00:45:18 -0700

    cbc

--- lib/screens/bottom_navigation_bar_screens/home_screen.dart
+++ lib/screens/bottom_navigation_bar_screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:haindi_graph/constants/global_variables.dart';
 import 'package:haindi_graph/screens/search/search_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,6 +10,33 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Color> list = [
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.yellow,
+    Colors.orange,
+  ];
+
+  Widget buildCircularImage(String imageUrl) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white,
+      ),
+      padding: const EdgeInsets.all(8),
+      margin: const EdgeInsets.all(8),
+      child: ClipOval(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,10 +46,8 @@ class _HomeScreenState extends State<HomeScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             IconButton(
-              icon: Icon(Icons.menu, color: Colors.black),
-              onPressed: () {
-                // Handle menu button pressed
-              },
+              icon: const Icon(Icons.menu, color: Colors.black),
+              onPressed: () {},
             ),
             const CircleAvatar(
               radius: 20,
@@ -33,14 +59,124 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
-          border: Border.all(width: .5, color: Colors.grey),
+          border: Border.all(width: 0.5, color: Colors.grey),
           color: const Color(0x7cdee5e5),
         ),
-        child: const Padding(
-          padding: EdgeInsets.all(15),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              SearchScreen(),
+              const SizedBox(
+                height: 5,
+              ),
+              const SearchScreen(),
+              const SizedBox(
+                height: 25,
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: GridView.builder(
+                        itemCount: list.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 11,
+                          mainAxisSpacing: 11,
+                        ),
+                        itemBuilder: (context, index) {
+                          return ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              bottomRight: Radius.circular(30),
+                            ),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: list[index],
+                              ),
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                    child: Image.network(
+                                      'https://th.bing.com/th/id/OIP.YAXlTjvtEKchdMVc5laZhwHaE8?pid=ImgDet&rs=1',
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  Container(
+                                    color:
+                                        const Color.fromARGB(255, 43, 95, 45),
+                                    height: 40,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        const Text(
+                                          'Under',
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                        Row(
+                                          children: const [
+                                            Text(
+                                              '₹ 499',
+                                              style: TextStyle(
+                                                  color: Colors.white),
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    Container(
+                      width: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          width: 2,
+                          color: const Color.fromARGB(255, 34, 84, 36),
+                        ),
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          bottomLeft: Radius.circular(30),
+                        ),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Color.fromARGB(255, 30, 80, 32),
+                            spreadRadius: .4,
+                            blurRadius: .4,
+                            offset: Offset(0, .4),
+                          )
+                        ],
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          buildCircularImage(
+                              'https://cpimg.tistatic.com/03447145/b/4/Wooden-Handicrafts-Elephant.jpeg'),
+                          buildCircularImage(
+                              'https://5.imimg.com/data5/TC/DE/MY-20031763/handicarft-itme-1000x1000.jpeg'),
+                          buildCircularImage(
+                              'https://m.media-amazon.com/images/I/61UQu6t2JaL._SL1500_.jpg'),
+                          buildCircularImage(
+                              'https://i.etsystatic.com/34592816/r/il/ace084/3856116303/il_794xN.3856116303_99sd.jpg'),
+                          buildCircularImage(
+                              'https://www.ulcdn.net/images/products/399704/slide/666x363/Daisy_Figurine_Multicolour_2.jpg?1632826902'),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
         ),
